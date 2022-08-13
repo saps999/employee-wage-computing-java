1c5e4fb91d6c372c1837a88a00a49f2236d338b0
saps999 <saptarshibiswasgang@gmail.com>
2022-08-13 18:23:13 +0530

    added logic for calculating employee's fulltime wage

--- src/EmployeeWageProgram.java
+++ src/EmployeeWageProgram.java
@@ -1,9 +1,14 @@
 public class EmployeeWageProgram {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE PROGRAM");
+        int wagePerHour = 20;
+        int fullTimeHour =8;
+        int dailyWage=0;
         int empcheck = (int) (Math.random() * (2));
         if (empcheck == 1) {
             System.out.println("Employee is present");
+            dailyWage = wagePerHour * fullTimeHour ;
+            System.out.println(dailyWage);
         } else {
             System.out.println("Employee is absent");
         }
