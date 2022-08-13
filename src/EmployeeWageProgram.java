38042edc8c82b245b486d1a7361959342a3f135d
saps999 <saptarshibiswasgang@gmail.com>
2022-08-13 18:36:11 +0530

    added logic for calculating employee's parttime wage

--- src/EmployeeWageProgram.java
+++ src/EmployeeWageProgram.java
@@ -2,12 +2,17 @@ public class EmployeeWageProgram {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE PROGRAM");
         int wagePerHour = 20;
-        int fullTimeHour =8;
+        int fullTimeHour = 8;
+        int partTimeHour = 4;
         int dailyWage=0;
-        int empcheck = (int) (Math.random() * (2));
+        int empcheck = (int) (Math.random() * ((2)+1));
         if (empcheck == 1) {
-            System.out.println("Employee is present");
+            System.out.println("Employee is full day present");
             dailyWage = wagePerHour * fullTimeHour ;
+            System.out.println(dailyWage);
+        } else if (empcheck == 2) {
+            System.out.println("Employee is half day present");
+            dailyWage = wagePerHour * partTimeHour;
             System.out.println(dailyWage);
         } else {
             System.out.println("Employee is absent");
