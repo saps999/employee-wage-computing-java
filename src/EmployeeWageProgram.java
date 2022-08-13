f084f38ce4668fd111cfee7a49a5f57400fd2e86
saps999 <saptarshibiswasgang@gmail.com>
2022-08-13 18:11:52 +0530

    added logic for checking employee present or absent

--- src/EmployeeWageProgram.java
+++ src/EmployeeWageProgram.java
@@ -1,5 +1,11 @@
 public class EmployeeWageProgram {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE PROGRAM");
+        int empcheck = (int) (Math.random() * (2));
+        if (empcheck == 1) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
