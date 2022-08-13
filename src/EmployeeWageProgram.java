cdc6041bec15f280000409d00c078a26c5aff392
saps999 <saptarshibiswasgang@gmail.com>
2022-08-13 18:46:07 +0530

    added case theory to check employee wage

--- src/EmployeeWageProgram.java
+++ src/EmployeeWageProgram.java
@@ -6,16 +6,30 @@ public class EmployeeWageProgram {
         int partTimeHour = 4;
         int dailyWage=0;
         int empcheck = (int) (Math.random() * ((2)+1));
-        if (empcheck == 1) {
-            System.out.println("Employee is full day present");
-            dailyWage = wagePerHour * fullTimeHour ;
-            System.out.println(dailyWage);
-        } else if (empcheck == 2) {
-            System.out.println("Employee is half day present");
-            dailyWage = wagePerHour * partTimeHour;
-            System.out.println(dailyWage);
-        } else {
-            System.out.println("Employee is absent");
+        switch (empcheck) {
+            case 1:
+                System.out.println("Employee is full day present");
+                dailyWage = wagePerHour * fullTimeHour ;
+                System.out.println(dailyWage);
+                break;
+            case 2:
+                System.out.println("Employee is half day present");
+                dailyWage = wagePerHour * partTimeHour;
+                System.out.println(dailyWage);
+                break;
+            default:
+                System.out.println("Employee is absent");
+        }
+    //    if (empcheck == 1) {
+    //        System.out.println("Employee is full day present");
+    //        dailyWage = wagePerHour * fullTimeHour ;
+    //        System.out.println(dailyWage);
+    //    } else if (empcheck == 2) {
+    //        System.out.println("Employee is half day present");
+    //        dailyWage = wagePerHour * partTimeHour;
+    //        System.out.println(dailyWage);
+    //    } else {
+    //        System.out.println("Employee is absent");
         }
     }
-}
+
