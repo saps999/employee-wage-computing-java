5af181948b28addfeaed92c5be435fe4845ef433
saps999 <saptarshibiswasgang@gmail.com>
2022-08-14 21:45:08 +0530

    Added while-loop for computing total working hours & day of the employee

--- src/EmployeeWageProgram.java
+++ src/EmployeeWageProgram.java
@@ -4,23 +4,29 @@ public class EmployeeWageProgram {
         int wagePerHour = 20;
         int fullTimeHour = 8;
         int partTimeHour = 4;
+        int workingHours =0;
         int dailyWage = 0;
-        int WorkingDays = 20;
-        for (int day = 0; day <= WorkingDays; day++) {
+        int workingDays = 20;
+        int totalWorkingDays = 0;
+    //  for (int day = 0; day < WorkingDays; day++) {
 
 
+        while (workingHours < 100 && totalWorkingDays < 20) {
             int empcheck = (int) (Math.random() * ((2) + 1));
+            totalWorkingDays ++;
             switch (empcheck) {
                 case 1:
-                    System.out.println("Employee is full day present " +day);
+                    workingHours = workingHours + fullTimeHour;
+                    System.out.println("Employee is full day present " );
                     dailyWage = dailyWage + wagePerHour * fullTimeHour;
                     break;
                 case 2:
-                    System.out.println("Employee is half day present " +day);
+                    workingHours = workingHours + partTimeHour;
+                    System.out.println("Employee is half day present " );
                     dailyWage = dailyWage + wagePerHour * partTimeHour;
                     break;
                 default:
-                    System.out.println("Employee is absent " +day);
+                    System.out.println("Employee is absent " );
             }
             //    if (empcheck == 1) {
             //        System.out.println("Employee is full day present");
@@ -33,6 +39,7 @@ public class EmployeeWageProgram {
             //    } else {
             //        System.out.println("Employee is absent");
 //       }
+            System.out.println("Working days: " + totalWorkingDays + " Hours: " + workingHours);
         }
         System.out.println("Employee's total wage for month is: " +dailyWage );
     }
