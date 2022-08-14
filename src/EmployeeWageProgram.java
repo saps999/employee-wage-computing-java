e04c10df9b50125ed701653d06c34f4a0cd29d6f
saps999 <saptarshibiswasgang@gmail.com>
2022-08-14 21:19:31 +0530

    Added for-loop for computing employee's wage per month

--- src/EmployeeWageProgram.java
+++ src/EmployeeWageProgram.java
@@ -4,32 +4,36 @@ public class EmployeeWageProgram {
         int wagePerHour = 20;
         int fullTimeHour = 8;
         int partTimeHour = 4;
-        int dailyWage=0;
-        int empcheck = (int) (Math.random() * ((2)+1));
-        switch (empcheck) {
-            case 1:
-                System.out.println("Employee is full day present");
-                dailyWage = wagePerHour * fullTimeHour ;
-                System.out.println(dailyWage);
-                break;
-            case 2:
-                System.out.println("Employee is half day present");
-                dailyWage = wagePerHour * partTimeHour;
-                System.out.println(dailyWage);
-                break;
-            default:
-                System.out.println("Employee is absent");
-        }
-    //    if (empcheck == 1) {
-    //        System.out.println("Employee is full day present");
-    //        dailyWage = wagePerHour * fullTimeHour ;
-    //        System.out.println(dailyWage);
-    //    } else if (empcheck == 2) {
-    //        System.out.println("Employee is half day present");
-    //        dailyWage = wagePerHour * partTimeHour;
-    //        System.out.println(dailyWage);
-    //    } else {
-    //        System.out.println("Employee is absent");
+        int dailyWage = 0;
+        int WorkingDays = 20;
+        for (int day = 0; day <= WorkingDays; day++) {
+
+
+            int empcheck = (int) (Math.random() * ((2) + 1));
+            switch (empcheck) {
+                case 1:
+                    System.out.println("Employee is full day present " +day);
+                    dailyWage = dailyWage + wagePerHour * fullTimeHour;
+                    break;
+                case 2:
+                    System.out.println("Employee is half day present " +day);
+                    dailyWage = dailyWage + wagePerHour * partTimeHour;
+                    break;
+                default:
+                    System.out.println("Employee is absent " +day);
+            }
+            //    if (empcheck == 1) {
+            //        System.out.println("Employee is full day present");
+            //        dailyWage = wagePerHour * fullTimeHour ;
+            //        System.out.println(dailyWage);
+            //    } else if (empcheck == 2) {
+            //        System.out.println("Employee is half day present");
+            //        dailyWage = wagePerHour * partTimeHour;
+            //        System.out.println(dailyWage);
+            //    } else {
+            //        System.out.println("Employee is absent");
 //       }
+        }
+        System.out.println("Employee's total wage for month is: " +dailyWage );
     }
 }
